4a4edfa4a5a20caa93aa9d6357fbe9ec48ba1a81
Joe Anderson <joe@mousetrapped.co.uk>
2020-06-03 18:02:23 +0100

    Add State::Core functions for working with StateIO

--- lib/rubio/state_core.rb
+++ lib/rubio/state_core.rb
@@ -3,10 +3,24 @@ module Rubio
     module Core
       extend Expose
       extend Unit::Core
+      extend IO::Core
+      extend Functor::Core
 
       State = ->(f) {
         pureIdentity = ->(x) { Monad::Identity::IdentityClass.pure(x) }
         StateClass.new(pureIdentity << f)
+      }
+
+      StateIO = ->(f) {
+        StateClass.new(f, monad_state_klass: IO)
+      }.curry
+
+      liftIO = ->(x) {
+        StateIO[ ->(s) {
+          x >> ->(a) {
+            pureIO[[a, s]]
+          }
+        }]
       }
 
       pureState = ->(x) {
@@ -15,13 +29,29 @@ module Rubio
         }]
       }
 
+      pureStateIO = ->(x) {
+        StateIO[ ->(s) {
+          pureIO[[x, s]]
+        }]
+      }
+
       get = State[ ->(s) {
         [s, s]
+      }]
+
+      getIO = StateIO[ ->(s) {
+        pureIO[ [s, s] ]
       }]
 
       put = ->(x) {
         State[ ->(s) {
           [unit, x]
+        }]
+      }
+
+      putIO = ->(x) {
+        StateIO[ ->(s) {
+          pureIO[ [unit, x] ]
         }]
       }
 
@@ -31,9 +61,21 @@ module Rubio
         }
       }
 
+      modifyIO = ->(f) {
+        getIO >> ->(x) {
+          putIO[ f[x] ]
+        }
+      }
+
       gets = ->(f) {
         get >> ->(x) {
           pureState[ f[x] ]
+        }
+      }
+
+      getsIO = ->(f) {
+        getIO >> ->(x) {
+          pureStateIO[ f[x] ]
         }
       }
 
@@ -41,23 +83,45 @@ module Rubio
         state.run[initial_state].value
       }.curry
 
+      runStateT = ->(state, initial_state) {
+        state.run[initial_state]
+      }.curry
+
       evalState = ->(act) {
         proc(&:first) << runState[act]
+      }
+
+      evalStateT = ->(act) {
+        fmap[proc(&:first)] << runStateT[act]
       }
 
       execState = ->(act) {
         proc(&:last) << runState[act]
       }
 
+      execStateT = ->(act) {
+        fmap[proc(&:last)] << runStateT[act]
+      }
+
       expose :State, State
+      expose :StateIO, StateIO
+      expose :liftIO, liftIO
       expose :pureState, pureState
+      expose :pureStateIO, pureStateIO
       expose :get, get
+      expose :getIO, getIO
       expose :put, put
+      expose :putIO, putIO
       expose :modify, modify
+      expose :modifyIO, modifyIO
       expose :gets, gets
+      expose :getsIO, getsIO
       expose :runState, runState
+      expose :runStateT, runStateT
       expose :evalState, evalState
+      expose :evalStateT, evalStateT
       expose :execState, execState
+      expose :execStateT, execStateT
     end
   end
 end
--- test/state_core_test.rb
+++ test/state_core_test.rb
@@ -3,26 +3,54 @@ require "test_helper"
 class StateCoreTest < Minitest::Test
   include Rubio::State::Core
   include Rubio::Unit::Core
+  include Rubio::IO::Core
 
   test "State :: (s -> (a, s)) -> State s a" do
     state = State[ ->(x) { [x, x] } ]
     assert_instance_of Rubio::State::StateClass, state
   end
 
-  test "pure :: a -> State s a" do
+  test "StateIO :: (s -> IO (a, s)) -> StateT s IO a" do
+    state = StateIO[ ->(x) { pureIO[[x, x]] } ]
+    assert_instance_of Rubio::State::StateClass, state
+  end
+
+  test "liftIO :: IO a -> StateT s IO a" do
+    state = liftIO[ pureIO[5] ]
+
+    assert_equal 5, evalStateT[state]["whatever"].perform!
+  end
+
+  test "pureState :: a -> State s a" do
     state = pureState[5]
 
     assert_equal [5, "Hello"], runState[state]["Hello"]
+  end 
+
+  test "pureStateIO :: a -> StateT s IO a" do
+    state = pureStateIO[5]
+
+    assert_equal [5, "whatever"], runStateT[state]["whatever"].perform!
   end 
 
   test "get :: State s s" do
     assert_equal [5, 5], runState[get][5]
   end
 
+  test "getIO :: StateT s IO s" do
+    assert_equal [5, 5], runStateT[getIO][5].perform!
+  end
+
   test "put :: s -> State s ()" do
     state = put["5"]
     
     assert_equal [unit, "5"], runState[state]["anything"]
+  end
+
+  test "putIO :: s -> StateT s IO ()" do
+    state = putIO["5"]
+    
+    assert_equal [unit, "5"], runStateT[state]["anything"].perform!
   end
 
   test "modify :: (s -> s) -> State s ()" do
@@ -33,6 +61,14 @@ class StateCoreTest < Minitest::Test
     assert_equal [unit, 42], runState[state][21]
   end
 
+  test "modifyIO :: (s -> s) -> State s IO ()" do
+    double = ->(x) { x * 2 }
+
+    state = modifyIO[double]
+
+    assert_equal [unit, 42], runStateT[state][21].perform!
+  end
+
   test "gets :: (s -> a) -> State s a" do
     double = ->(x) { x * 2 }
 
@@ -41,6 +77,13 @@ class StateCoreTest < Minitest::Test
     assert_equal [10, 5], runState[state][7]
   end
 
+  test "getsIO :: (s -> a) -> State s a" do
+    double = ->(x) { x * 2 }
+
+    state = putIO[5] >> getsIO[double]
+
+    assert_equal [10, 5], runStateT[state][7].perform!
+  end
 
   test "runState -> State s a -> s -> (a, s)" do
     state = push[1] >> push[2] >> push[3] >> pop
@@ -48,15 +91,54 @@ class StateCoreTest < Minitest::Test
     assert_equal [3, [2, 1, 10, 11]], runState[state][ [10, 11] ]
   end
 
+  test "runStateIO -> State s IO a -> s -> IO (a, s)" do
+    state = pushIO[1] >> pushIO[2] >> pushIO[3] >> popIO
+
+    assert_equal [3, [2, 1, 10, 11]], runStateT[state][ [10, 11] ].perform!
+  end
+
   test "evalState :: State s a -> s -> a" do
     state = push[1] >> push[2] >> push[3] >> pop
+
     assert_equal 3, evalState[state][ [10, 11] ]
+  end
+
+  test "evalStateIO :: State s IO a -> s -> IO a" do
+    state = pushIO[1] >> pushIO[2] >> pushIO[3] >> popIO
+
+    assert_equal 3, evalStateT[state][ [10, 11] ].perform!
   end
 
   test "execState :: State s a -> s -> s" do
     state = push[1] >> push[2] >> push[3] >> pop
 
     assert_equal [2, 1, 10, 11], execState[state][ [10, 11] ]
+  end
+
+  test "execStateT :: State s IO a -> s -> s" do
+    state = pushIO[1] >> pushIO[2] >> pushIO[3] >> popIO
+
+    assert_equal [2, 1, 10, 11], execStateT[state][ [10, 11] ].perform!
+  end
+
+  test "StateIO cumulatively binds lifted IOs" do
+    state = popIO >> ->(x) {
+      (liftIO << println)["I just popped #{x}"] >>
+      popIO >> ->(y) {
+        (liftIO << println)["I just popped #{y}"] >>
+        pushIO[x] >>
+        pushIO[y] >>
+        getIO >> ->(s) {
+          (liftIO << println)["The state is now #{s.inspect}"]
+        }
+      }
+    }
+
+    io = evalStateT[state][ [1, 2, 3, 4, 5] ]
+
+    assert_output "I just popped 1\nI just popped 2\nThe state is now [2, 1, 3, 4, 5]\n" do
+      io.perform!
+    end
   end
 
   private
@@ -75,6 +157,23 @@ class StateCoreTest < Minitest::Test
 
     gets[head] >> ->(x) {
       modify[tail] >> pureState[x]
+    }
+  end
+
+  def pushIO
+    ->(x) {
+      modifyIO[ ->(xs) {
+        [x] + xs
+      }]
+    }
+  end
+
+  def popIO
+    head = proc(&:first)
+    tail = ->(xs) { xs.drop(1) }
+
+    getsIO[head] >> ->(x) {
+      modifyIO[tail] >> pureStateIO[x]
     }
   end
 end
